509d020846cf2dbea2589dcb604487a46c98fc47
maroos <maroos.vp@gmail.com>
2025-07-14 21:14:50 +0200

    dockerfile update to latest zephyr v4.1.0 and SDK 0.17.0

--- tools/start_docker.sh
+++ tools/start_docker.sh
@@ -5,9 +5,9 @@
 
 if [ $1 ]
 then
-  exec docker run --rm -it -v "$(pwd)"/workspace:/workspace -w /workspace --device=$1:$1 env-zephyr-espressif
+  exec docker run --rm -it -v "$(pwd)"/workspace:/workspace -w /workspace --device=$1:$1 env-zephyr-espressif:v4.1
 else
-  exec docker run --rm -it -v "$(pwd)"/workspace:/workspace -w /workspace env-zephyr-espressif
+  exec docker run --rm -it -v "$(pwd)"/workspace:/workspace -w /workspace env-zephyr-espressif:v4.1
 fi
 
 #  exec docker run --rm -it -v "$(pwd)"/workspace:/workspace -w /workspace --device=/dev/ttyUSB0:/dev/ttyUSB0 env-zephyr-espressif
--- tools/template_build_esp32.sh
+++ tools/template_build_esp32.sh
@@ -3,11 +3,17 @@
 # Start docker container for zephyr with connected device
 #!/bin/bash
 
-if [ $1 ]
+if [ $1 = "m" ]
 then
-  exec "weird arg"
+  west build -p always -b esp32_devkitc_wroom/esp32/procpu -- -DDTC_OVERLAY_FILE=boards/esp32_devkitc_wroom.overlay
+  west flash
+  exec west espressif monitor
+elif [ $1 = 'f' ]
+then
+  west build -p always -b esp32_devkitc_wroom/esp32/procpu -- -DDTC_OVERLAY_FILE=boards/esp32_devkitc_wroom.overlay
+  exec west flash
 else
-  exec west build -p always -b esp32_devkitc_wroom/esp32/procpu -- -DDTC_OVERLAY_FILE=boards/esp32_devkitc_wroom.overlay
+  west build -p always 
 fi
 
 
