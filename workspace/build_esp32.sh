ed40af163aebac16e6a92d2e384e7634e03c4f16
maroos <maroos.vp@gmail.com>
2025-07-14 12:15:21 +0200

    cpp examples added with configuration and build script changed

--- workspace/build_esp32.sh
+++ workspace/build_esp32.sh
@@ -8,9 +8,12 @@ then
   west build -p always -b esp32_devkitc_wroom/esp32/procpu -- -DDTC_OVERLAY_FILE=boards/esp32_devkitc_wroom.overlay
   west flash
   exec west espressif monitor
-else
+elif [ $1 = 'f' ]
+then
   west build -p always -b esp32_devkitc_wroom/esp32/procpu -- -DDTC_OVERLAY_FILE=boards/esp32_devkitc_wroom.overlay
   exec west flash
+else
+  west build -p always 
 fi
 
 
